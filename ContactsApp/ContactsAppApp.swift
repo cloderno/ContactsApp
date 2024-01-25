929fdf2f6395eada0d3694f0ed41b42b679592a8
cloderno <cloderno@gmail.com>
2024-01-25 23:36:47 +0600

    Initial Commit

---
+++ ContactsApp/ContactsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsAppApp.swift
+//  ContactsApp
+//
+//  Created by Rating on 25.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct ContactsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
